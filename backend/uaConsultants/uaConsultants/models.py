08a3b23bcf4e7ec4de7da1a99c5a636088bac644
Charlie Dunstan <35953283+cwdunstan@users.noreply.github.com>
2019-09-16 18:24:11 +1000

    Update(backend): views with http responses

--- backend/uaConsultants/uaConsultants/models.py
+++ backend/uaConsultants/uaConsultants/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class conversation(models.Model):
-    conversations = models.CharField(max_length=50)
+class conversationData(models.Model):
+    business_id = models.CharField(max_length=50)
     business_name = models.CharField(max_length=50)
     business_picture = models.CharField(max_length=100)
     job_link = models.CharField(max_length=50)
--- backend/uaConsultants/uaConsultants/views.py
+++ backend/uaConsultants/uaConsultants/views.py
@@ -1,17 +1,31 @@
 from django.http import HttpResponse 
-from .models import conversation
+from django.http import JsonResponse
+import json
+from .models import conversationData
 
 def home(request):
-    return HttpResponse('home')
+    if request.method == 'GET':
+        return HttpResponse('200')
+    elif request.method == 'POST':
+        return HttpResponse('400')
 
 def user(request):
-    return HttpResponse('user')
+    if request.method == 'GET':
+        return HttpResponse('200')
+    elif request.method == 'POST':
+        return HttpResponse('400')
 
 def job(request):
-    return HttpResponse('job')
+    if request.method == 'GET':
+        return HttpResponse('200')
+    elif request.method == 'POST':
+        return HttpResponse('400')
 
 def chat(request):
-    chat_data = conversation.objects.all()
-    for field in chat_data:
-        print(field.conversations+' '+field.business_name+' '+field.business_picture+' '+field.job_link+' '+field.user_name)
-    return HttpResponse('chat')
+    if request.method == 'GET':
+        response_data=[]
+        for field in conversationData.objects.all():
+            response_data.append(field.business_id)
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+    elif request.method == 'POST':
+    return HttpResponse('400')

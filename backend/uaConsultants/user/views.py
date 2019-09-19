6d315fb9dc143627bd256ce17a1b368f1246e66e
Mir <mmah4946@uni.sydney.edu.au>
2019-09-19 21:13:30 +1000

    JWT auth and login

--- backend/uaConsultants/user/views.py
+++ backend/uaConsultants/user/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib import auth
 from django.http import HttpResponse 
 from django.http import JsonResponse
 import json

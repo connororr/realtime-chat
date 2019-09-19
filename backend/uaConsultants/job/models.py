9a488df53a68a26e56abcd0582e60cb7cc92cef2
Connor <20336835+WriteCode433@users.noreply.github.com>
2019-09-19 23:40:04 +1000

    added models for job and jobPhoto

--- backend/uaConsultants/job/models.py
+++ backend/uaConsultants/job/models.py
@@ -1,5 +1,16 @@
-from django.db import models
+from djongo import models
 
+
+class JobPhoto(models.Model):
+    image = models.CharField(max_length=300)
+    title = models.CharFIeld(max_length=50)
 
 class Job(models.Model):
+    job_name = models.CharField(max_length=50)
+    date_created = models.IntegerField()
     job_summary = models.CharField(max_length=50)
+    business_id = models.CharField(max_length=50)
+    current_bid = models.IntegerField()
+    project_photos = models.ArrayModelField(
+        model_container = JobPhoto
+    )

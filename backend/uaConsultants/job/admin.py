891e2e848a1db682c87b0040a1e0c83b93320260
root <root@DESKTOP-00C1B1G.localdomain>
2019-10-09 10:36:16 +1100

    feat(backend): job/view/<job_id> working, adding job auto increments job_id. Currently has foreign key referencing username (as primary key of user table) may need to change to business name

--- backend/uaConsultants/job/admin.py
+++ backend/uaConsultants/job/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import job
 
-admin.site.register(job)
+class jobAdmin(admin.ModelAdmin):
+    exclude = ('business_name',)
+    list_display = ('job_name', 'job_description', 'date_created')
+    
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.business_name = request.user
+        obj.save()
+
+admin.site.register(job, jobAdmin)
+
--- backend/uaConsultants/job/models.py
+++ backend/uaConsultants/job/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from django.conf import settings
+from user.models import CustomUser
+import datetime
+
 
 # class JobPhoto(models.Model):
 #     image = models.CharField(max_length=300)
 #     title = models.CharField(max_length=50)
 
 class job(models.Model):
-
     job_name = models.CharField(max_length=50,default='My Job')
-    date_created = models.TextField(max_length=500, blank=True)
-    job_summary = models.TextField(max_length=500, blank=True)
-    business_id = models.TextField(max_length=500, blank=True)
+    job_description = models.TextField(max_length=500, blank=True)
+    date_created = models.DateTimeField(default=datetime.datetime.now)
+    business_name = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.PROTECT)
+    job_location = models.TextField(max_length=500, blank=True)
     current_bid = models.TextField(max_length=500, blank=True)
+    bid_amount = models.TextField(max_length=500, blank=True)
     project_photos = models.ImageField(blank=True)
 
     def __str__(self):
-        return "{}".format(self.job_name)
+      return "{}".format(self.job_name)

c094b0bc58d8a0ef2c52d05e4f579a99db248976
charlie <cdun4036@uni.sydney.edu.au>
2019-10-10 16:47:39 +1100

    feat(backend):error handling

--- backend/uaConsultants/job/urls.py
+++ backend/uaConsultants/job/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     
     # path('search/', views.jobSearch),
     # path('photo/upload/', views.jobPhotoUpload),
-    #path('register/', views.jobRegister),
-     path('view', views.jobView),
+    path('register', views.jobRegister),
+    path('view', views.jobView),
     # path('bid/', views.jobBid),
     
 ] 
--- backend/uaConsultants/job/views.py
+++ backend/uaConsultants/job/views.py
@@ -10,14 +10,29 @@ from . import models, permissions
 from . import serializers
 
 
-# #### /Job Responses ###
+##### /Job/view Responses ###
 def jobView(request):
-    req_dict = dict()
-    userID=""
     if request.method == 'GET':
+        #load http request body into python dictionary, req_dict
         req_dict = json.loads(request.body)  
         userID = req_dict['user_id']
-        serialized_qs = serializers.jobSerialize(models.job.objects.get(business_id=userID))
-        serialized_qs.data
-    return HttpResponse(JSONRenderer().render(serialized_qs.data), content_type='application/json')
+        #query database for user_id, if found return data via jobSerializer. 
+        try:
+            serialized_qs = serializers.jobSerialize(models.job.objects.get(business_id=userID))
+            serialized_qs.data
+            return HttpResponse(JSONRenderer().render(serialized_qs.data), content_type='application/json')
+        except:
+            return JsonResponse({'error':'lookup failed','status':'failure'}, status=404)
+    elif request.method == 'POST':
+        return JsonResponse({'error':'invalid request','status':'failure'}, status=404)
+
+def jobRegister(request):
+    if request.method == 'POST':
+        # req_dict = json.loads(request.body)  
+        # for i in req_dict:
+        #     print(i)
+        return JsonResponse({'error':'invalid request','status':'failure'}, status=404)
+    elif request.method == 'GET':
+        return JsonResponse({'error':'invalid request','status':'failure'}, status=404)
+
 

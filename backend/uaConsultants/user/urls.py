44a16ef0d055443a5132f5b872c7944be4672d2e
Mir <mmah4946@uni.sydney.edu.au>
2019-10-14 19:14:21 +1100

    backend: no csrf

--- backend/uaConsultants/user/urls.py
+++ backend/uaConsultants/user/urls.py
@@ -5,7 +5,7 @@ from allauth.account.views import confirm_email
 from django.conf.urls import url
 import re 
 from rest_auth.registration.views import VerifyEmailView, RegisterView
-
+from django.views.generic import TemplateView
 from rest_auth.views import (
     LoginView, LogoutView, PasswordChangeView,
     PasswordResetView, PasswordResetConfirmView
@@ -13,13 +13,18 @@ from rest_auth.views import (
 
 urlpatterns = [
     
-    path('login', LoginView.as_view(), name="user-login"),
-    path('logout', LogoutView.as_view(), name='user-logout'),
+    path('login', views.LoginViewCustom.as_view(), name="user-login"),
+    path('logout', views.LogoutViewCustom.as_view(), name='user-logout'),
     path('password/change', PasswordChangeView.as_view(), name='rest_password_change'),
     path('password/reset', PasswordResetView.as_view(), name='rest_password_reset'),
     path('password/reset/confirm', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
     path('profile',views.CustomUserDetailsView.as_view()),
     path('profile/all', views.allUserView.as_view()),
-    path('register', include('rest_auth.registration.urls')),
+    path('register', views.RegisterViewCustom.as_view()),
+    path('register/verify-email', views.VerifyEmailViewCustom.as_view()),
+    # path('account-confirm-email/(?P<key>[-:\w]+)/$', TemplateView.as_view(), name='account_confirm_email')
+    url(r'^account-confirm-email/(?P<key>[-:\w]+)/$', TemplateView.as_view(),
+        name='account_confirm_email'),
+
 ]
 

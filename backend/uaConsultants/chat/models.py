1254ce3ace0e4fe8fd622eaa0107ec02683dbbd1
Connor <20336835+WriteCode433@users.noreply.github.com>
2019-10-15 14:49:18 +1100

    added conversation & msg models, serializers & admin page

--- backend/uaConsultants/chat/models.py
+++ backend/uaConsultants/chat/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.conf import settings
 from user.models import CustomUser
 import datetime
 
+
 class Conversation(models.Model):
-    business = models.ForeignKey(CustomUser,on_delete=models.PROTECT)
+    sender = models.ForeignKey(CustomUser,on_delete=models.PROTECT, related_name='sender')
+    receiver = models.ForeignKey(CustomUser, on_delete=models.PROTECT, related_name='receiver')
     job_link = models.TextField(max_length=500, blank=True)
 
 class Message(models.Model):
-    conversation = models.ForeignKey(Conversation,on_delete=models.PROTECT)
+    conversation = models.ForeignKey(Conversation,on_delete=models.PROTECT, related_name='messages')
     user_id = models.ForeignKey(CustomUser,on_delete=models.PROTECT)
     message = models.TextField(max_length=500, blank=True)
     time_sent = models.DateTimeField(auto_now=False,auto_now_add=True)
--- backend/uaConsultants/chat/serializers.py
+++ backend/uaConsultants/chat/serializers.py
@@ -2,8 +2,10 @@ from rest_framework import serializers
 from . import models
 from user.serializers import UserSerializer
 
+
+
 class MessageSerialize(serializers.ModelSerializer):
-    user_id = serializers.CharField(source='bussiness.id', read_only=True)
+    user_id = serializers.CharField(source='user_id.id', read_only=True)
 
     class Meta:
         model = models.Message
@@ -12,10 +14,13 @@ class MessageSerialize(serializers.ModelSerializer):
 
 class ConversationSerialize(serializers.ModelSerializer):
     messages = MessageSerialize(many=True, read_only=True)
-    business_name = serializers.CharField(source='business.business_name', read_only=True)
-    user_name = serializers.CharField(source='business.user_name', read_only=True)
-    profile_picture = serializers.CharField(source='business.profile_picture', read_only=True)
+    sender_business_name = serializers.CharField(source='sender.business_name', read_only=True)
+    sender_user_name = serializers.CharField(source='sender.name', read_only=True)
+    sender_profile_picture = serializers.CharField(source='sender.profile_picture', read_only=True)
+    receiver_business_name = serializers.CharField(source='receiver.business_name', read_only=True)
+    receiver_user_name = serializers.CharField(source='receiver.name', read_only=True)
+    receiver_profile_picture = serializers.CharField(source='receiver.profile_picture', read_only=True)
 
     class Meta:
         model = models.Conversation
-        fields = ('user_name','business_name', 'profile_picture','job_link','conversation_id','messages')
+        fields = ('sender_user_name','sender_business_name', 'sender_profile_picture','receiver_user_name','receiver_business_name', 'receiver_profile_picture','job_link','id','messages')

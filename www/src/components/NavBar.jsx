32ee02e480b647522e7a77598cc3da5117703982
Ben <pseudoramdev@gmail.com>
2019-10-22 00:41:17 +1100

    fix(www): landing page rebuild

--- www/src/components/NavBar.jsx
+++ www/src/components/NavBar.jsx
@@ -7,8 +7,10 @@ import Logo from './Logo';
 const Wrapper = styled.div`
   height: 50px;
   width: 100%;
-  background: #317ee3;
   color: #333333;
+  position: fixed;
+  top: 0;
+  left: 0;
   height: 72px;
   display: flex;
   justify-content: space-between;
@@ -53,78 +55,52 @@ const buttonStyles = `
   }
 `;
 
-const Button = styled(Link)`
-  ${buttonStyles}
-`;
-
 const ButtonBasic = styled.button`
   ${buttonStyles}
 `;
 
-const SpecialButton = styled(Link)`
-  background-color: #f0932b;
-  border-bottom-color: rgba(0, 0, 0, 0);
-  border-bottom-left-radius: 8px;
-  border-bottom-right-radius: 8px;
-  border-bottom-style: solid;
-  border-bottom-width: 2px;
-  border-image-outset: 0;
-  border-image-repeat: stretch;
-  border-image-slice: 100%;
-  border-image-source: none;
-  border-image-width: 1;
-  border-left-color: rgba(0, 0, 0, 0);
-  border-left-style: solid;
-  border-left-width: 2px;
-  border-right-color: rgba(0, 0, 0, 0);
-  border-right-style: solid;
-  border-right-width: 2px;
-  border-top-color: rgba(0, 0, 0, 0);
-  border-top-left-radius: 8px;
-  border-top-right-radius: 8px;
-  border-top-style: solid;
-  border-top-width: 2px;
-  box-sizing: border-box;
-  color: rgb(255, 255, 255);
-  cursor: pointer;
-  display: inline-block;
-  font-family: 'Raleway', sans-serif;
-  font-size: 14.4px;
-  font-weight: 400;
-  letter-spacing: 1.5px;
-  line-height: 21.6px;
-  max-width: 100%;
-  outline-color: rgb(255, 255, 255);
-  outline-style: none;
-  outline-width: 0px;
-  padding-bottom: 5px;
-  padding-left: 20px;
-  padding-right: 20px;
-  padding-top: 5px;
-  position: relative;
-  text-align: center;
+const buttonStyle = `
+  color: #1f2430;
   text-decoration: none;
-  text-decoration-color: rgb(255, 255, 255);
-  text-decoration-line: none;
+  font-size: 14px;
+  font-weight: 500;
+  padding: 0 15px;
+  cursor: pointer;
+`;
 
-  &:hover {
-    opacity: 0.8;
-  }
+const Button = styled(Link)`
+  ${buttonStyle}
+`;
+
+const SpecialButton = styled(Link)`
+  ${buttonStyle}
 `;
 
 const LogoWrapper = styled(Logo)`
   cursor: pointer;
 `;
 
+const RightButton = styled.div`
+  right: 22px;
+  position: absolute;
+`;
+
+const LeftButton = styled.div`
+  left: 129px;
+  position: absolute;
+`;
+
 const isLoggedIn = () => localStorage.getItem('session') !== null;
 
-const loggedOutPages = [{ name: 'Log In', special: true, location: '/login' }];
+const pages = [
+  { name: 'Find projects', location: '/search' },
+  { name: 'Browse businesses', location: '/search' },
+];
 
-const loggedInPages = [
-  { name: 'Search', special: false, location: '/search' },
-  { name: 'Messages', special: false, location: '/conversations' },
-  { name: 'Profile', special: false, location: '/profile' },
-  { name: 'Bids', special: false, location: '/profile/bids' },
+const pagesLocked = [
+  { name: 'Messages', location: '/conversations' },
+  { name: 'Profile', location: '/profile' },
+  { name: 'Bids', location: '/profile/bids' },
 ];
 
 const NavBar = ({ active, style }) => (
@@ -134,14 +110,28 @@ const NavBar = ({ active, style }) => (
         <LogoWrapper />
       </Link>
       <Buttons>
-        {(isLoggedIn() ? loggedInPages : loggedOutPages).map(page =>
-          page.special ? (
-            <SpecialButton to={page.location}>{page.name}</SpecialButton>
-          ) : (
+        <LeftButton>
+          {pages.map(page => (
             <Button to={page.location}>{page.name}</Button>
-          )
+          ))}
+          {isLoggedIn() &&
+            pagesLocked.map(page => (
+              <Button to={page.location}>{page.name}</Button>
+            ))}
+        </LeftButton>
+        {!isLoggedIn() && (
+          <RightButton>
+            <SpecialButton to="/login">For supplier</SpecialButton>{' '}
+            <SpecialButton
+              to="/login"
+              style={{ borderLeft: '1px solid #1f24303b' }}
+            >
+              Sign in
+            </SpecialButton>
+          </RightButton>
         )}
-        {isLoggedIn() ? (
+
+        {isLoggedIn() && (
           <ButtonBasic
             style={{
               width: 'max-content',
@@ -157,7 +147,7 @@ const NavBar = ({ active, style }) => (
               {localStorage.getItem('b_name')}
             </span>
           </ButtonBasic>
-        ) : null}
+        )}
       </Buttons>
     </Content>
   </Wrapper>

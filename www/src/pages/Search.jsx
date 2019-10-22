32b9c5fb838b6585db12b0c7579bfea2493a2d0a
Ben <pseudoramdev@gmail.com>
2019-10-23 02:12:50 +1100

    grid to list update

--- www/src/pages/Search.jsx
+++ www/src/pages/Search.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from '@reach/router';
 import styled from 'styled-components';
 import Filters from '../components/Filters';
 import axios from 'axios';
 import JobCard from '../components/JobCard';
 import SearchBar from '../components/SearchBar';
+import { FiGrid, FiList } from 'react-icons/fi';
+import JobListItem from '../components/JobListItem';
 
 const ContentWrapper = styled.div`
   height: calc(100vh - 72px);
@@ -31,57 +33,82 @@ const SearchResults = styled.div`
   flex-direction: row;
 `;
 
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
 const StyledSearch = styled(SearchBar)`
   margin: 0 auto;
 `;
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      results: [],
-    };
-  }
+const ResultsHolder = styled.div`
+  max-width: 780px;
+  width: 100%;
+`;
 
-  componentDidMount() {
-    this.getResults();
-  }
+const RefineBar = styled.div`
+  width: 100%;
+  display: flex;
+`;
 
-  getResults = () => {
-    const self = this;
-    axios.get('http://localhost:3800/job/search').then(response => {
-      const results = response.data.results.map(result => (
-        <Link to={`/project/${result['business_id']}`}>
-          <JobCard
-            project={result['project_name']}
-            desc={result.description}
-            key={result['business_id']}
-            b_id={result['business_id']}
-            bid={result['current_bid']}
-            b_name={result['business_name']}
-            location={result['location']}
-            image={result['project_photo'].image}
-            alt={result['project_photo'].title}
-          />
-        </Link>
-      ));
+const Buttons = styled.div``;
 
-      self.setState({ results });
-    });
-  };
+const getResults = setResults => {
+  axios.get('http://localhost:3800/job/search').then(response => {
+    setResults(response.data.results);
+  });
+};
 
-  render() {
-    const { results } = this.state;
-    return (
-      <ContentWrapper>
-        <SearchWrapper>
-          <StyledSearch />
-        </SearchWrapper>
-        <Filters />
-        <SearchResults>{results}</SearchResults>
-      </ContentWrapper>
-    );
-  }
-}
+const Search = () => {
+  const [results, setResults] = useState([]);
+  const [sortOption, setSortOption] = useState(0);
+  const [arrangement, setArrangement] = useState(1);
+
+  useEffect(() => {
+    getResults(setResults);
+  });
+
+  return (
+    <ContentWrapper>
+      <SearchWrapper>
+        <StyledSearch />
+      </SearchWrapper>
+      <RefineBar>
+        <Buttons>
+          <FiList />
+          <FiGrid />
+        </Buttons>
+      </RefineBar>
+      <Wrapper>
+        <ResultsHolder>
+          {results.map(result => (
+            <Link
+              to={`/project/${result['business_id']}`}
+              style={{ textDecoration: 'none' }}
+            >
+              {arrangement === 0 ? (
+                <JobCard
+                  project={result['project_name']}
+                  desc={result.description}
+                  key={result['business_id']}
+                  b_id={result['business_id']}
+                  bid={result['current_bid']}
+                  b_name={result['business_name']}
+                  location={result['location']}
+                  image={result['project_photo'].image}
+                  alt={result['project_photo'].title}
+                />
+              ) : (
+                <JobListItem />
+              )}
+            </Link>
+          ))}
+        </ResultsHolder>
+      </Wrapper>
+    </ContentWrapper>
+  );
+};
 
 export default Search;
